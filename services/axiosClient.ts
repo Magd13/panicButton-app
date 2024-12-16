84bbc24727dd9381596dd90ebf835000425145e8
danit232 <daniela.tarapues@relatividadia.com>
2024-12-16 09:18:13 -0500

    Pantalla d historia d alertas

--- services/axiosClient.ts
+++ services/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const apiClient: AxiosInstance = axios.create({
-    baseURL: 'http://192.168.100.4:8080',
+    baseURL: 'http://192.168.100.94:8080',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
